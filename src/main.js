fb823819572cb3271fde37934b00625b976e117e
Julissa <julissa.2606@gmail.com>
2021-02-15 12:01:26 -0500

    HU 2

--- src/main.js
+++ src/main.js
@@ -1,71 +1,113 @@
-// import athletes from './data/athletes/athletes.js';
-// import athletes from './data/athletes/athletes.js';
-// import data from './data/athletes/athletes.js';
+const viewAthletes = document.getElementById('viewAthletes');
 
-// const data = './data/athletes/athletes.js';
-// const res = '';
-// fetch({data}).then(res => athletes.js())
-// // .catch(error => console.error('Error:', error))
-// // .then(response => console.log('Success:', response));
-// console.log(res);
-// console.log(example, data);
+const search = document.getElementById('search');
+const inputSearch = document.getElementById('inputSearch');
 
-// fetch('./data/athletes/athletes.js')
-// .then( answer => {return data.results} )
-
-// .then(data =>{
-//     // console.log(data.results)
-//     data.results.forEach(element => {
-//         console.log(element.name);
-//     });
-// });
-// document.write(Object.values(athletes.athletes));
-
-
-
-
-
-const obtenerAthletes = async() => {
-        
+// consumo de la API
+document.addEventListener("DOMContentLoaded", e => {
+    // le pasamos lo que queremos cargar cuando se ingrese a nuestro sitio web
+    obtenerAthletes();
+})
+ 
+// Como haremos una consulta exterior por la API trabajaremos con ASYNC Y AWAIT
+const obtenerAthletes = async () => {
+    try {
         const res = await fetch ('./data/athletes/athletes.json')
+        const resCountries = await fetch ('./data/athletes/countriesFlag.json')
         const data = await res.json()
-        // console.log(data)    
-        // const arrayAthletes = data.athletes.map(athletes => athletes)   
+        const dataCountriesFlag = await resCountries.json()
+        const data1 = data.athletes.map(athletes => athletes)   
+        const arrayMedalGold = data1.filter(athletes => athletes.medal === 'Gold') 
+        const arrayCountries = data1.map(athletes => athletes.team);
+        // console.log(dataCountries);
+        dataAthletes(arrayMedalGold)
+        // dataAthletes1(data1);
+        searchAthletes(data1,arrayMedalGold);   
+        // dataCountries(dataCountriesFlag);
         const arrayAthletes = data.athletes.filter(athletes => athletes.name) 
-        const arrayMedalGold = data.athletes.filter(athletes => athletes.medal === 'Gold') 
         const arrayMedalBronze = data.athletes.filter(athletes => athletes.medal === 'Bronze')
         const arrayAthletesUsain = data.athletes.filter(athletes => athletes.name === 'Usain St. Leo Bolt') 
         const arrayName = data.athletes.map(athletes => athletes.name)
         const arrayNoc = data.athletes.map(athletes => athletes.noc)
         const arraySport = data.athletes.map(athletes => athletes.sport)
-        //data.athletes.filter(athletes => console.log(athletes))
-        // console.log(arrayName);
-        // console.log(arrayNoc);
-        // console.log(arraySport);
-        // console.log(arrayAthletes[0]);
-        // console.log(arrayName[1])
-        // console.log(arrayMedalGold);
-        // console.log(arrayMedalBronze);
-
-        // const Usain = arrayAthletesUsain.toString();
-        // console.log(console.log(Usain));
-
-        // document.getElementById('mostrarAtletas').innerHTML.name = Usain;
-
-        for (let a=0; a<arrayAthletesUsain.length; a++){
-            // document.getElementById('mostrarAtletas').innerHTML = arrayAthletesUsain[a];
-            document.getElementById('mostrarAtletas').innerHTML = `<p>${arrayAthletesUsain[a].name}</p><br>
-            <p>${arrayAthletesUsain[a].noc}</p> <br> <p>${arrayAthletesUsain[a].sport}</p>`;
-            console.log(arrayAthletesUsain[a]);
-            // prompt(arrayAthletesUsain[a]);
-        }
-
         console.log(arrayAthletes[0].name);
-        // } catch (error) {
-        //     console.log(error)
-        // }
 
-        
+
+
+    } catch (error) {
+        console.log(error);
     }
+}
 
- obtenerAthletes()
+
+const dataAthletes = arrayMedalGold => {
+    let elementos = '';
+    
+    // console.log(medalGoldAthletes);
+    for (let i = 0; i < 21; i++) {
+         elementos += `
+        <article class="card">
+            <div class="card-content">
+                <h4>${arrayMedalGold[i].name.toUpperCase()}</h4>
+                <p>
+                    ${arrayMedalGold[i].noc } &nbsp|&nbsp ${arrayMedalGold[i].sport.toUpperCase()}
+                </p>
+                <p>
+                    <a href="pais.html?name=${arrayMedalGold[i].games}">Mas info</a>
+                </p>
+            </div>
+        </article>
+        `
+    }
+    viewAthletes.innerHTML = elementos;
+    // console.log(arrayMedalGold);
+}
+ 
+
+const dataAthletes1 = data1 => {
+    let elementos = '';
+
+    data1.forEach(item => {
+        elementos += `
+        <article class="card">
+            <div class="card-content">
+                <h4>${item.name.toUpperCase()}</h4>
+                <p>
+                    ${item.noc} &nbsp|&nbsp ${item.sport.toUpperCase()}
+                </p>
+                <p>
+                    <a href="pais.html?name=${item.games}">Mas info</a>
+                </p>
+            </div>
+        </article>
+        `
+    });
+    viewAthletes.innerHTML = elementos;
+}
+
+
+ const searchAthletes = (data1,arrayMedalGold) => {
+    search.addEventListener('keyup', e => {
+        e.preventDefault()
+        const textUser = inputSearch.value.toLowerCase();
+        // console.log(textUser);
+        // console.log(typeof(textUser));
+        
+        const arrayTeam = data1.filter(item => {
+            const itemTeam = item.name.toLowerCase();
+            // console.log(itemTeam);
+            if(itemTeam.indexOf(textUser)!== -1 ){
+                 return item;
+            } 
+        })
+        
+        if (arrayTeam.length == 2023) {
+            dataAthletes(arrayMedalGold);
+        } else {
+            dataAthletes1(arrayTeam);
+        }
+    });
+ }
+
+
+
