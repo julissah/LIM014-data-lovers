f2ab46281fee94d5ad5752e5b8f530ddfa7a8af5
Julissa <julissa.2606@gmail.com>
2021-02-22 08:39:47 -0500

    HU2-Order Alpha

--- src/data.js
+++ src/data.js
@@ -1,13 +1,165 @@
-// estas funciones son de ejemplo
-import data from './data/athletes/athletes.json';
+const viewCountries = document.getElementById('mainCountries');
+const viewAthletes = document.getElementById('mainAthletes');
+const searchC = document.getElementById('searchC');
+const searchA = document.getElementById('searchA');
 
-export const example = (data) => {
+// *** Countries
+export const countries = (dataCountries,dataCountriesFlag) =>{
+  const sorted = dataCountries.sort();
+  const unique = sorted.filter((value,index) => {
+      return value != sorted[index + 1];
+  });
+  console.log(unique)
+  let elementos = '';
   
-  return data;
-};
+  for (let i = 0; i < unique.length; i++) {
+      for (let f = 0; f < dataCountriesFlag.length; f++) {    
+          if (dataCountriesFlag[f].alpha3Code.indexOf(unique[i])!== -1) {
+          // console.log(dataCountriesFlag);
+          elementos += `
+          <article class="card">
+          <div class="card-content">
+              <img src=${dataCountriesFlag[f].flag} alt="" class="img-fluid">  
+              <h4>${dataCountriesFlag[f].name}</h4>
+              <!--<h4>${dataCountriesFlag[f].alpha3Code}</h4>-->
+              <!--<h4>${unique[i]}</h4>-->
+          </div>
+          </article>
+          `
+      } 
+  }    
+}
+viewCountries.innerHTML = elementos;
+}
 
-// export const anotherExample = () => {
-//   return 'OMG';
-// };
+// Filtro de busqueda Countries
+
+export const searchCountries = dataCountriesFlag => {
+    const inputSearchCountries = document.getElementById('inputSearchCountries');
+
+    searchC.addEventListener('keyup', e => {
+        e.preventDefault()
+        const textUser = inputSearchCountries.value.toLowerCase();
+        // console.log(textUser);
+        // console.log(typeof(textUser));
+        
+        const arraySearchCountries = dataCountriesFlag.filter(item => {
+            const itemTeam = item.name.toLowerCase();
+            console.log(itemTeam);
+            if(itemTeam.indexOf(textUser)!== -1 ){
+                 return item;
+            } 
+        })
+        viewSearchCountries(arraySearchCountries);
+    });
+ }
+
+ export const viewSearchCountries = arraySearchCountries => {
+    let elementos2 = '';
+    arraySearchCountries.forEach(item => {
+        elementos2 += `
+        <article class="card">
+            <div class="card-content">
+                <img src=${item.flag} alt="" class="img-fluid">  
+                <h4>${item.name}</h4>
+            </div>
+        </article>
+        `
+    });
+        viewCountries.innerHTML = elementos2;
+}
+
+// Filtro Alpha
+// export const orderAlphaZA = (arraySearchCountries) => {
+//     const orderAlpha = document.getElementById('orderAlpha');
+//     orderAlpha.addEventListener('click',() => {
+        
+//     })
+//     return ;
+// }
 
 
+export const orderAlpha = (option, arrayMedalGold) => {
+    
+    switch (option) {
+      case "1":
+        arrayMedalGold.sort((a, b) => a.name.localeCompare(b.name));
+        athletesGold(arrayMedalGold);
+        // arrayMedalGold.sort;
+        break;
+
+      default:
+        arrayMedalGold.sort((a, b) => a.name.localeCompare(b.name));
+        const reversed = arrayMedalGold.reverse();
+        athletesGold(reversed)
+    }
+    return arrayMedalGold;
+  };
+
+
+
+// *** Athletes
+export const athletesGold = arrayMedalGold => {
+    let elementos = '';    
+    // console.log(medalGoldAthletes);
+    for (let i = 0; i < 20; i++) {
+         elementos += `
+        <article class="card">
+            <div class="card-content">
+                <img src="./img/logo.png" alt="" class="img">
+                <h4>${arrayMedalGold[i].name.toUpperCase()}</h4>
+                <p>
+                    ${arrayMedalGold[i].noc } &nbsp|&nbsp ${arrayMedalGold[i].sport.toUpperCase()}
+                </p>
+                <p>
+                    <a href="pais.html?name=${arrayMedalGold[i].games}">Mas info</a>
+                </p>
+            </div>
+        </article>
+        `
+    }
+    viewAthletes.innerHTML = elementos;
+    // console.log(arrayMedalGold);
+}
+
+export const searchAthletes = (dataRio,arrayMedalGold) => {
+    const inputSearchAthletes = document.getElementById('inputSearchAthletes');
+    searchA.addEventListener('keyup', e => {
+        e.preventDefault()
+        const textUser = inputSearchAthletes.value.toLowerCase();
+        console.log(textUser);
+        const arrayTeam = dataRio.filter(item => {
+            const itemTeam = item.name.toLowerCase();
+            // console.log(itemTeam.length);
+            if(itemTeam.indexOf(textUser)!== -1 ){
+                 return item;
+            } 
+        })
+        if (arrayTeam.length == 2023) {    
+            athletesGold(arrayMedalGold);
+        } else {
+            athletesAll(arrayTeam);
+        }
+    });
+ }
+
+export const athletesAll = dataRio => {
+    let elementos = '';
+    dataRio.forEach(item => {
+        elementos += `
+        <article class="card">
+            <div class="card-content">
+                <img src="./img/logo.png" alt="" class="img">
+                <h4>${item.name.toUpperCase()}</h4>
+                <p>
+                    ${item.noc} &nbsp|&nbsp ${item.sport.toUpperCase()}
+                </p>
+                <p>
+                    <a href="pais.html?name=${item.games}">Mas info</a>
+                </p>
+            </div>
+        </article>
+        `
+    });
+    viewAthletes.innerHTML = elementos;
+}
--- src/main.js
+++ src/main.js
@@ -1,115 +1,44 @@
-const viewAthletes = document.getElementById('viewAthletes');
+import { 
+countries,    
+searchCountries,
+athletesGold,
+searchAthletes,
+orderAlpha,
 
-const search = document.getElementById('search');
-const inputSearchAthletes = document.getElementById('inputSearchAthletes');
+} from './data.js';
 
-// consumo de la API
-document.addEventListener("DOMContentLoaded", e => {
-    // le pasamos lo que queremos cargar cuando se ingrese a nuestro sitio web
-    obtenerAthletes();
+import data from './data/athletes/athletes.js';
+import dataFlag from './data/athletes/flag.js';
+// import athletes from './data/athletes/athletes.js';
+
+addEventListener('DOMContentLoaded', () => {
+const dataRio = data.athletes.map(athletes =>athletes) //data athletes
+const dataCountriesFlag = dataFlag.map(dataFlag => dataFlag) //data countries flag
+const dataCountriesNoc = data.athletes.map(athletes => athletes.noc) //data athletes noc
+const arrayMedalGold = dataRio.filter(athletes => athletes.medal === 'Gold')
+
+
+countries(dataCountriesNoc,dataCountriesFlag);
+searchCountries(dataCountriesFlag);
+athletesGold(arrayMedalGold)
+searchAthletes(dataRio,arrayMedalGold)
+// orderAlpha(arrayMedalGoldName)
+// console.log(dataCountries,dataCountriesFlag)
+
+const alpha = document.getElementById("orderAlpha");
+const resultText = document.getElementById("result");
+console.log(alpha);
+alpha.addEventListener("change", (e) => {
+    const arrayOrder = e.target.value;
+    orderAlpha(arrayOrder, arrayMedalGold);
+    console.log(arrayOrder)
+  });
 })
- 
-// Como haremos una consulta exterior por la API trabajaremos con ASYNC Y AWAIT
-const obtenerAthletes = async () => {
-    try {
-        const res = await fetch ('./data/athletes/athletes.json')
-        const resCountries = await fetch ('./data/athletes/countriesFlag.json')
-        const data = await res.json()
-        const dataCountriesFlag = await resCountries.json()
-        const data1 = data.athletes.map(athletes => athletes)   
-        const arrayMedalGold = data1.filter(athletes => athletes.medal === 'Gold') 
-        const arrayCountries = data1.map(athletes => athletes.team);
-        // console.log(dataCountries);
-        dataAthletes(arrayMedalGold)
-        // dataAthletes1(data1);
-        searchAthletes(data1,arrayMedalGold);   
-        // dataCountries(dataCountriesFlag);
-        const arrayAthletes = data.athletes.filter(athletes => athletes.name) 
-        const arrayMedalBronze = data.athletes.filter(athletes => athletes.medal === 'Bronze')
-        const arrayAthletesUsain = data.athletes.filter(athletes => athletes.name === 'Usain St. Leo Bolt') 
-        const arrayName = data.athletes.map(athletes => athletes.name)
-        const arrayNoc = data.athletes.map(athletes => athletes.noc)
-        const arraySport = data.athletes.map(athletes => athletes.sport)
-        console.log(arrayAthletes[0].name);
 
 
 
-    } catch (error) {
-        console.log(error);
-    }
-}
+// Filtro de Ordenar
 
-
-const dataAthletes = arrayMedalGold => {
-    let elementos = '';
-    
-    // console.log(medalGoldAthletes);
-    for (let i = 0; i < 21; i++) {
-         elementos += `
-        <article class="card">
-            <div class="card-content">
-                <img src="./img/logo.png" alt="" class="img">
-                <h4>${arrayMedalGold[i].name.toUpperCase()}</h4>
-                <p>
-                    ${arrayMedalGold[i].noc } &nbsp|&nbsp ${arrayMedalGold[i].sport.toUpperCase()}
-                </p>
-                <p>
-                    <a href="pais.html?name=${arrayMedalGold[i].games}">Mas info</a>
-                </p>
-            </div>
-        </article>
-        `
-    }
-    viewAthletes.innerHTML = elementos;
-    // console.log(arrayMedalGold);
-}
- 
-
-const dataAthletes1 = data1 => {
-    let elementos = '';
-
-    data1.forEach(item => {
-        elementos += `
-        <article class="card">
-            <div class="card-content">
-                <h4>${item.name.toUpperCase()}</h4>
-                <p>
-                    ${item.noc} &nbsp|&nbsp ${item.sport.toUpperCase()}
-                </p>
-                <p>
-                    <a href="pais.html?name=${item.games}">Mas info</a>
-                </p>
-            </div>
-        </article>
-        `
-    });
-    viewAthletes.innerHTML = elementos;
-}
-
-
- const searchAthletes = (data1,arrayMedalGold) => {
-    search.addEventListener('keyup', e => {
-        e.preventDefault()
-        const textUser = inputSearchAthletes.value.toLowerCase();
-        console.log(textUser);
-        
-        const arrayTeam = data1.filter(item => {
-            const itemTeam = item.name.toLowerCase();
-            // console.log(itemTeam.length);
-            if(itemTeam.indexOf(textUser)!== -1 ){
-                 return item;
-            } 
-        })
-        
-        if (arrayTeam.length == 2023) {
-            
-            dataAthletes(arrayMedalGold);
-        } else {
-            dataAthletes1(arrayTeam);
-        }
-        // dataAthletes1(arrayTeam);
-    });
- }
 
 
 
